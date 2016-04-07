ee7bcdbc4c0f30bc0441ec719fa0c6b8587f3adf
mengtuoling111 <1157345877@qq.com>
2016-04-07 14:08:31 +0800

    cq

---
+++ java7_practise_biz/src/main/java/com/java7/current/model/Event.java
@@ -0,0 +1,28 @@
+package com.java7.current.model;
+
+import java.util.Date;
+
+/**
+ * Created by lenovo on 2016/4/7.
+ */
+public class Event {
+
+    private Date date;
+    private String event;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+}
