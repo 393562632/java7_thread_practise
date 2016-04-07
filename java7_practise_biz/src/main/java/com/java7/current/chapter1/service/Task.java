6e4adc0f15d5a34100328d7263a16d32ab3d4370
mengtuoling111 <1157345877@qq.com>
2016-04-07 16:22:37 +0800

    cq

---
+++ java7_practise_biz/src/main/java/com/java7/current/chapter1/service/Task.java
@@ -0,0 +1,11 @@
+package com.java7.current.chapter1.service;
+
+/**
+ * Created by lenovo on 2016/4/7.
+ */
+public class Task implements Runnable {
+    public void run() {
+        int number = Integer.parseInt("TTTT");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++");
+    }
+}
